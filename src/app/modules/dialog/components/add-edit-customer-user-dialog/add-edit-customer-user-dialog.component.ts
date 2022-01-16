b10b537b4c4184c28dbf545ed7f14a860865e80e
RFUERTES\Rfuertes-LP <reynelfuertes@gmail.com>
2022-01-16 17:52:06 +0800

    ui changes

--- src/app/modules/dialog/components/add-edit-customer-user-dialog/add-edit-customer-user-dialog.component.ts
+++ src/app/modules/dialog/components/add-edit-customer-user-dialog/add-edit-customer-user-dialog.component.ts
@@ -24,14 +24,22 @@ export class AddEditCustomerUserDialogComponent implements OnInit {
   public $access: Observable<IAccess[]>;
   public $roles: Observable<ISimpleItem[]>;
   public selectedCustomerUser: ICustomerUser;
-
+  public userPreSelectedAccess: string[] = [
+    'a6ded0f0-af1e-471d-afe0-3891308d2bb3', //agreements
+    '27a51b15-5587-4380-a2ca-21296e0a9b3e', // assessments
+    '057ee2e2-cb7c-4e05-aea8-0f2b962ea150', //venues
+    'e684b0c0-6450-44e5-b83c-d07470be8584', // products
+    'b3401f0e-5495-45c3-87b7-1cf3abe7eac7' //tags
+  ];
+  public userPreSelectedRole: string[] = ['2a69d8c5-3434-4ab9-ba74-bfb465c09d05'];
+  
   constructor(private store: Store<RootState>, private fb: FormBuilder, public dialogRef: MatDialogRef<AddEditCustomerUserDialogComponent>, @Inject(MAT_DIALOG_DATA) public data: any) {
     this.form = this.fb.group({
       id: [null],
       username: [`user${Math.floor(Math.random() * (1000 - 100) + 1000)}@gmail.com`, Validators.compose([Validators.required, Validators.pattern(emailRegex.email)])],
       password: [generatePassword(), Validators.required],
-      roles: [null, Validators.required],
-      accesses: [null, Validators.required],
+      roles: [this.userPreSelectedRole, Validators.required],
+      accesses: [this.userPreSelectedAccess, Validators.required],
       profile: [null]
     });
 
